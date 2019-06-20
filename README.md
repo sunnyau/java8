68d0a678013af0f602ef63681db9f2df191b3c15
Sunny Au <sunny.au@outlook.com>
2019-06-20 22:20:47 +0100

    add mvn component-tests profile and src/it/java structure

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 My Java exercise
+
+Run 'mvn compile test' run tests in src/test/java
+Run 'mvn verify -Pcomponent-tests' only run tests in src/it/java
+
+
---
+++ src/it/java/tests/TestRunner.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class TestRunner {
+
+    @Test
+    public void mavenComponentTestsProfileCallThis() {
+        assertTrue(true);
+    }
+}
